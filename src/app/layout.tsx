5e78c527017a4215bd15c6b4bf647f825509a760
Deyvid Dimitrov <davida1991@gmail.com>
2026-05-20 20:13:08 +0300

    Restore navigation sticky header by removing body level overflow and containing it inside main element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-brand-light text-brand-dark">
         <Header />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow overflow-x-hidden">{children}</main>
         <Footer />
       </body>
     </html>
